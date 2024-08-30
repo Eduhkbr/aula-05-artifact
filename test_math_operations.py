c73643ee83c3b05fe059e2e896712f2ae4f8da4c
Eduhkbr <47605719+Eduhkbr@users.noreply.github.com>
2024-08-30 15:21:40 +0000

    ajuste python

--- test_math_operations.py
+++ test_math_operations.py
@@ -1,9 +1,9 @@
-# test_math_operations.py
-
 import unittest
 from math_operations import is_prime
 
+
 class TestMathOperations(unittest.TestCase):
+
     def test_is_prime_positive(self):
         self.assertTrue(is_prime(2))
         self.assertTrue(is_prime(3))
@@ -15,6 +15,7 @@ class TestMathOperations(unittest.TestCase):
         self.assertFalse(is_prime(4))
         self.assertFalse(is_prime(9))
         self.assertFalse(is_prime(15))
+
 
 if __name__ == '__main__':
     unittest.main()
